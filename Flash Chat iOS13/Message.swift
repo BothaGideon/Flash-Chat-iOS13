1d32d444666037ac5836e5eab35bb8957ef151b2
Gideon Botha <gideon.botha@entelect.co.za>
2021-05-03 17:17:48 +0200

    changed a lot of stuff

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Gideon Botha on 2021/05/03.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
